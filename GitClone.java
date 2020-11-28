7c5a5f20174f726535eac88086e4086e4a8ec8fa
MP092707 <MP092707@5CD6383857-W10.morganlewis.net>
2020-11-27 21:04:36 -0500

    Added second code

--- GitClone.java
+++ GitClone.java
@@ -2,7 +2,7 @@
 public class GitClone {
 
 	public static void main(String[] args) {
-		System.out.println("This is Shamrose");
+		System.out.println("This is Shamrose; Can't pull a request");
 
 	}
 
