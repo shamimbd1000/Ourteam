c68773b3c6e53a62c18190fe35ba3727cb009e01
chitaarun <pratay.das08@gmail.com>
2020-12-07 00:16:28 -0500

    # WARNING: head commit changed in the meantime
    This is for git hub test
    this is array

---
+++ NewTeam/Arial.java
@@ -0,0 +1,28 @@
+package rahul;
+
+public class Arial {
+	
+     public static void main(String[] args) {
+    	 
+    	 int a[][]= new int[2][3];
+    	 
+    	 a[0][0]=2;
+    	 a[0][1]=4;
+    	 a[0][2]=5;
+    	 
+    	 a[1][0]=3;
+    	 a[1][1]=4;
+    	 a[1][2]=7;
+    	 
+    	 for(int i=0; i<2; i++)
+    	 {
+    		 
+    		 for(int j=0; j<3; j++)
+    		 {
+    		 	 System.out.println(a[i][j]);
+    	    			 
+    		 }  	 
+}
+}
+     
+}
