04829e91878b8778c73f00dcdba2b278b809df1c
chitaarun <pratay.das08@gmail.com>
2020-12-08 23:21:55 -0500

    It is about date.

---
+++ NewTeam/Dateprint.java
@@ -0,0 +1,16 @@
+package rahul;
+
+import java.util.Date;
+
+public class Dateprint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Date d= new Date();
+		
+		System.out.println(d.toString());
+	}
+
+}
